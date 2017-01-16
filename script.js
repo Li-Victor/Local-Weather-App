fe8d03e496224ae92eadf2ddb36781b73823a5fe
victorli5674 <victorli5674@yahoo.com>
2017-01-15 17:06:24 -0800

    connected with openweathermap api. Able to get the local city temperature and description

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 "use strict";
 $(document).ready(function() {
+    var URL = 'http://api.openweathermap.org/data/2.5/weather?appid=307bb89abee478e231ee1dba858e80b4';
+
+    //browsers geolocation
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            $("#data").append("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+            URL += '&lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;
+            //default temperature is in fahrenheit
+            var FahrenUrl = URL + '&units=imperial';
+            var jqxhr = $.getJSON(FahrenUrl)
+                .done(function(response) {
+                    $('#data').append(response.name + ' ' + response.weather[0].main + ' temperature is ' + response.main.temp );
+                })
+                .fail(function(response) {
+                    $('#data').append('Something has gone wrong');
+                })
+
+
         });
     }
+
 })
