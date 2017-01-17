1ad1c237cca90f7bebd818091ea2bc269f05ff6f
victorli5674 <victorli5674@yahoo.com>
2017-01-16 22:20:17 -0800

    implemented button to toggle between both fahrenheit and celcius temperatures

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 "use strict";
 $(document).ready(function() {
-    var URL = 'http://api.openweathermap.org/data/2.5/weather?appid=307bb89abee478e231ee1dba858e80b4';
-
     //browsers geolocation
     if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            URL += '&lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;
-            //default temperature is in fahrenheit
-            var FahrenUrl = URL + '&units=imperial';
-            var jqxhr = $.getJSON(FahrenUrl)
-                .done(function(response) {
-                    $('#data').append(response.name + ' ' + response.weather[0].main + ' temperature is ' + response.main.temp );
-                })
-                .fail(function(response) {
-                    $('#data').append('Something has gone wrong');
-                })
+        var URL = 'http://api.openweathermap.org/data/2.5/weather?appid=307bb89abee478e231ee1dba858e80b4';
+        var imperialTemp = true;
 
+        function getLocationURL(imperaialTemp) {
+            if(imperialTemp) {
+                return URL + '&units=imperial';
+            }
+            return URL + '&units=metric';
+        }
 
+        function displayLocationWeather() {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var finalURL = getLocationURL() + '&lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;
+
+                var jqxhr = $.getJSON(finalURL)
+                    .done(function(response) {
+                        $('#data').html(response.name + ' ' + response.weather[0].main + ' temperature is ' + response.main.temp );
+                    })
+                    .fail(function(response) {
+                        $('#data').html('Something has gone wrong');
+                    });
+            })
+        }
+
+        displayLocationWeather();
+
+        $('#switch').on('click', function() {
+            imperialTemp = !imperialTemp;
+            displayLocationWeather();
         });
+
+    } else {
+        $('#data').html('Please share location');
     }
 
 })
