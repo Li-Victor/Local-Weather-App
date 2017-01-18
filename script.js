f7b409eb03f3e9c7432a050a19b9454209fffd14
victorli5674 <victorli5674@yahoo.com>
2017-01-17 16:55:02 -0800

    optimized by having sharing location pressed once

--- script.js
+++ script.js
@@ -3,35 +3,47 @@ $(document).ready(function() {
     //browsers geolocation
     if(navigator.geolocation) {
         var URL = 'http://api.openweathermap.org/data/2.5/weather?appid=307bb89abee478e231ee1dba858e80b4';
-        var imperialTemp = true;
 
-        function getLocationURL(imperaialTemp) {
-            if(imperialTemp) {
-                return URL + '&units=imperial';
-            }
-            return URL + '&units=metric';
+        navigator.geolocation.getCurrentPosition(function(position) {
+            getUserLocation(position.coords.latitude, position.coords.longitude);
+        });
+
+        function getUserLocation(latitude, longitude) {
+
+            var jqxhr = $.getJSON(URL + '&lat=' + latitude +
+                            '&lon=' + longitude + '&units=imperial')
+                .done(function(response) {
+                    displayLocationWeather(response.name, response.weather[0].main, response.main.temp);
+
+                })
+                .fail(function(response) {
+                    $('#data').html('Something has gone wrong');
+                });
+
         }
 
-        function displayLocationWeather() {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var finalURL = getLocationURL() + '&lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;
-
-                var jqxhr = $.getJSON(finalURL)
-                    .done(function(response) {
-                        $('#data').html(response.name + ' ' + response.weather[0].main + ' temperature is ' + response.main.temp );
-                    })
-                    .fail(function(response) {
-                        $('#data').html('Something has gone wrong');
-                    });
-            })
+        var cityName;
+        var cityWeather;
+        var Ftemp;
+        var Ctemp;
+        var imperial;
+        function displayLocationWeather(city, weather, temperature) {
+            $('#data').html(city + ' ' + weather + ' temperature is ' + temperature + ' Fahrenheit');
+            cityName = city;
+            cityWeather = weather;
+            Ftemp = temperature;
+            Ctemp = (temperature - 32) * 5 / 9;
+            imperial = true;
         }
-
-        displayLocationWeather();
 
         $('#switch').on('click', function() {
-            imperialTemp = !imperialTemp;
-            displayLocationWeather();
-        });
+            imperial = !imperial;
+            if(imperial) {
+                $('#data').html(cityName + ' ' + cityWeather + ' temperature is ' + Ftemp + ' Fahrenheit');
+            } else {
+                $('#data').html(cityName + ' ' + cityWeather + ' temperature is ' + Ctemp + ' Celcius');
+            }
+        })
 
     } else {
         $('#data').html('Please share location');
